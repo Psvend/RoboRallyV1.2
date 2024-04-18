723b4074f4b22fb859b5f12191fcacc64ea7c8cf
Benjamin Philip Bakkelev <s235097@dtu.dk>
2024-04-18 14:05:41 +0200

    can save

---
+++ roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/model/PlayerTemplate.java
@@ -0,0 +1,11 @@
+package dk.dtu.compute.se.pisd.roborally.fileaccess.model;
+
+import dk.dtu.compute.se.pisd.roborally.model.Space;
+
+public class PlayerTemplate {
+    public String name;
+    public String color;
+
+    public Space space;
+
+}
