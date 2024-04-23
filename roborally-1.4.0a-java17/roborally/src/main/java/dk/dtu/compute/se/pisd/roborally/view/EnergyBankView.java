8d217ac9f31b8ec3f8e5f413bd191f007d4380d4
Petrine <s235064@dtu.dk>
2024-04-23 16:34:33 +0200

    label for energybank view

--- roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/view/EnergyBankView.java
+++ roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/view/EnergyBankView.java
@@ -1,30 +1,22 @@
 package dk.dtu.compute.se.pisd.roborally.view;
 import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 import dk.dtu.compute.se.pisd.designpatterns.observer.Subject;
 
+//VIRKER IKKE
+public class EnergyBankView extends VBox {
 
-public class EnergyBankView extends GridPane implements ViewObserver {
+  private Label energyBankLabel;
 
-    private Label energyLabel;
-    private EnergyBank energyBank;
+  public EnergyBankView energyBankView() {
+    EnergyBankView energyBankView = new EnergyBankView();
+    energyBankLabel = new Label("Energy Bank Level: " + EnergyBank.getBankStatus());
+    this.getChildren().add(energyBankLabel);
+    this.setStyle("-fx-padding: 10;");
+    energyBankLabel.setStyle("-fx-font-size: 16; -fx-font-weight: bold;");
+    return energyBankView;
+}
 
-
-    public EnergyBankView(EnergyBank energyBank) {
-
-        this.energyBank = energyBank;
-        energyLabel = new Label("Energy Bank Level: " + EnergyBank.getBankStatus());
-        this.add(energyLabel, 0, 0);
-        this.energyBank.attach(this);
-    
-
-    }
-
-
-    @Override
-    public void updateView(Subject subject) {
-        throw new UnsupportedOperationException("Unimplemented method 'updateView'");
-    }
- 
-}   
+}
 
