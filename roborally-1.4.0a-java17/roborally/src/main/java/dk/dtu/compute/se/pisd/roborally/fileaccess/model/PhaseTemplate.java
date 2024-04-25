2043cd596425a95ab4df6338ba5f37377c74c419
Benjamin Philip Bakkelev <s235097@dtu.dk>
2024-04-25 15:59:03 +0200

    Phasetemplate class made

--- roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/model/PhaseTemplate.java
+++ roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/model/PhaseTemplate.java
@@ -3,6 +3,13 @@ package dk.dtu.compute.se.pisd.roborally.fileaccess.model;
 public class PhaseTemplate {
     private String phase;
 
+    public PhaseTemplate() {
+    }
+
+    public PhaseTemplate(String phase) {
+        this.phase = phase;
+    }
+
     public String getPhase() {
         return phase;
     }
