d18162d77eaa7cae1e3fce51585158118b5752c3
Benjamin Philip Bakkelev <s235097@dtu.dk>
2024-04-25 15:04:25 +0200

    save cards now

--- roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/model/CommandCardFieldTemplate.java
+++ roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/model/CommandCardFieldTemplate.java
@@ -1,6 +1,26 @@
 package dk.dtu.compute.se.pisd.roborally.fileaccess.model;
 
+import dk.dtu.compute.se.pisd.roborally.model.CommandCard;
+
 public class CommandCardFieldTemplate {
     private boolean visible;
+
+    private CommandCard card;
     private String cardType;
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
+    }
+
+    public CommandCard getCard() {
+        return card;
+    }
+
+    public void setCard(CommandCard card) {
+        this.card = card;
+    }
 }
