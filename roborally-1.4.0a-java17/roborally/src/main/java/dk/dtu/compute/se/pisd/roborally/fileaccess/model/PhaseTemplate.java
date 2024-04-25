553815f37039b192e0f1210de63e395daf7a14d0
Benjamin Philip Bakkelev <s235097@dtu.dk>
2024-04-25 15:48:26 +0200

    cards

---
+++ roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/model/PhaseTemplate.java
@@ -0,0 +1,13 @@
+package dk.dtu.compute.se.pisd.roborally.fileaccess.model;
+
+public class PhaseTemplate {
+    private String phase;
+
+    public String getPhase() {
+        return phase;
+    }
+
+    public void setPhase(String phase) {
+        this.phase = phase;
+    }
+}
