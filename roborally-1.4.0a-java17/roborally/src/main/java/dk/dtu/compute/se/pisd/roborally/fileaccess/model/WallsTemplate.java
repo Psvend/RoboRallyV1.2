ecace3059388418ce7fa815b9857d0607f4e6c76
Benjamin Philip Bakkelev <s235097@dtu.dk>
2024-04-26 23:47:54 +0200

    template for walls

---
+++ roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/model/WallsTemplate.java
@@ -0,0 +1,26 @@
+package dk.dtu.compute.se.pisd.roborally.fileaccess.model;
+
+public class WallsTemplate {
+    public int x;
+    public int y;
+    public String heading;
+
+    public int getX() {
+        return x;
+    }
+    public void setX(int x) {
+        this.x = x;
+    }
+    public int getY() {
+        return y;
+    }
+    public void setY(int y) {
+        this.y = y;
+    }
+    public String getHeading() {
+        return heading;
+    }
+    public void setHeading(String heading) {
+        this.heading = heading;
+    }
+}
