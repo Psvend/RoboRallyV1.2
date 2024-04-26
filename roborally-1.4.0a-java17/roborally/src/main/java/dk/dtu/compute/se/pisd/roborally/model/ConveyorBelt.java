08cba253447f47486fd5de15c44ee7f6abcbc422
JalokinBo <Nikolajbo@gmail.com>
2024-04-26 12:02:54 +0200

    ConveyorBelt (ConveyorClass 1.2)

--- roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/model/ConveyorBelt.java
+++ roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/model/ConveyorBelt.java
@@ -3,6 +3,7 @@ package dk.dtu.compute.se.pisd.roborally.model;
 public class ConveyorBelt extends Space{
 
     private Heading heading;
+    private int beltType;
 
     public Heading getHeading() {
         return heading;
@@ -12,7 +13,18 @@ public class ConveyorBelt extends Space{
         this.heading = heading;
     }
 
-    public String setBeltType()
+    public int getBeltType(){
+        return beltType;
+    }
+
+    public void setBeltType(int beltType) {
+        if(beltType != this.beltType) {
+            this.beltType = beltType;
+            notifyChange();
+        }
+    }
+
+    FieldAction ConveyorBelt = 
 
     public ConveyorBelt(Board board, int x, int y) {
         super(board, x, y);
