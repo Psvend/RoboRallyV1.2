7facd9019680bd84efe80910634a370b7d2f70b0
JalokinBo <144294697+JalokinBo@users.noreply.github.com>
2024-04-28 22:27:47 +0200

    heyo blå

--- roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/Activator.java
+++ roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/Activator.java
@@ -1,17 +1,21 @@
 package dk.dtu.compute.se.pisd.roborally.controller;
 
+import org.jetbrains.annotations.NotNull;
+
 import dk.dtu.compute.se.pisd.roborally.model.Board;
 
 public class Activator {
     private Board board;
+    private GameController gameController;
 
-    private void activateConveyorBelt(Board board){
+    private void activateConveyorBelt(@NotNull Board board, @NotNull GameController gameController){
         this.board = board;
+        this.gameController = gameController;
         for(int x = 0; x < board.width; x++){
             for(int y = 0; y < board.height; y++) {
                 if(this.board.getSpace(x, y).getConveyorBelt() != null){
-
-                }
+                    this.board.getSpace(x, y).getConveyorBelt().doAction(this.gameController, this.board.getSpace(x, y));
+                } 
             }
         }
     }
