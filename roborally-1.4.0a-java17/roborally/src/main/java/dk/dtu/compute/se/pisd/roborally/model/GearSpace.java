6589e7eaf4dddaec30a913375f180d797603b81e
Louise Lund <s153976@dtu.dk>
2024-04-29 00:47:00 +0200

    GearSpace oprettet

--- roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/model/GearSpace.java
+++ roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/model/GearSpace.java
@@ -1,5 +1,17 @@
 package dk.dtu.compute.se.pisd.roborally.model;
 
-public class GearSpace {
+/**
+ * @author Louise
+ * The design of the gearspaces
+ */
+public class GearSpace  extends Space{
+
+    final public boolean startSpace;
+
+    public GearSpace (Board board, int x, int y) {
+        super(board, x, y);
+        this.startSpace = true;
+    }
+
     
 }
