06e73b8b6c3f6ab1eaa22c12d282e222a671e0de
JalokinBo <144294697+JalokinBo@users.noreply.github.com>
2024-04-28 23:27:38 +0200

    pls pls

--- roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/Activator.java
+++ roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/Activator.java
@@ -19,9 +19,9 @@ public class Activator {
     public void activateConveyorBelt(@NotNull Board board, @NotNull GameController gameController){
         this.board = board;
         this.gameController = gameController;
-        for(int x = 0; x < board.width; x++){
-            for(int y = 0; y < board.height; y++) {
-                if(this.board.getSpace(x, y).getConveyorBelt() != null){
+        for(int x = 0; x < this.board.width; x++){
+            for(int y = 0; y < this.board.height; y++) {
+                if(this.board.getSpace(x, y).getConveyorBelt() != null ){
                     this.board.getSpace(x, y).getConveyorBelt().doAction(this.gameController, this.board.getSpace(x, y));
                 } else {
                 }
