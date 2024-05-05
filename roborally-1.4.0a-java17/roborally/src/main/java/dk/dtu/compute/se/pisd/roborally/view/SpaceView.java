753bb7df3367789f73a6b55fb4508c0d2277e053
JalokinBo <144294697+JalokinBo@users.noreply.github.com>
2024-05-05 23:43:56 +0200

    Alt implementeret med gearSpaces :)

--- roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/view/SpaceView.java
+++ roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/view/SpaceView.java
@@ -24,6 +24,7 @@ package dk.dtu.compute.se.pisd.roborally.view;
 import dk.dtu.compute.se.pisd.designpatterns.observer.Subject;
 import dk.dtu.compute.se.pisd.roborally.model.*;
 import dk.dtu.compute.se.pisd.roborally.controller.ConveyorBelt;
+import dk.dtu.compute.se.pisd.roborally.controller.GearSpace;
 import dk.dtu.compute.se.pisd.roborally.model.Board;
 import dk.dtu.compute.se.pisd.roborally.model.Heading;
 import dk.dtu.compute.se.pisd.roborally.model.Player;
@@ -90,6 +91,12 @@ public class SpaceView extends StackPane implements ViewObserver {
             this.setId("energyspace-view");
         } else if(space instanceof PriorityAntenna){
             this.setId("priorityantenna-view");
+        } else if (space.getGearSpace() instanceof GearSpace){
+            if(space.getGearSpace().getGearType().equals("LEFT")){
+                this.setStyle("-fx-background-color: crimson;");
+            } else if(space.getGearSpace().getGearType().equals("Right")){
+                this.setStyle("-fx-background-color: chocolate;");
+            }
         } else if(space.getConveyorBelt() instanceof ConveyorBelt) {
             if(space.getConveyorBelt().getBeltType()==1){
                 if(space.getConveyorBelt().getTurnBelt().equals("LEFT")){
