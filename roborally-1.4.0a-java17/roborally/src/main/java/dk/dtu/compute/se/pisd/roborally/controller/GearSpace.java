6aa434b3c89d3ef7f8f47dad531ab5e60a8e579f
JalokinBo <144294697+JalokinBo@users.noreply.github.com>
2024-05-05 22:32:58 +0200

    GearSpace gameController class (done)

--- roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/GearSpace.java
+++ roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/GearSpace.java
@@ -5,6 +5,7 @@ import dk.dtu.compute.se.pisd.roborally.model.Space;
 
 public class GearSpace extends FieldAction{
     private Heading heading;
+    private String gearType;
 
     public Heading getHeading(){
         return heading;
@@ -14,6 +15,13 @@ public class GearSpace extends FieldAction{
         this.heading = heading;
     }
 
+    public String gearType(){
+        return gearType;
+    }
+
+    public void gearType(String gearType){
+        this.gearType = gearType;
+    }
     @Override
     public boolean doAction(GameController gameController, Space space) {
         // TODO Auto-generated method stub
