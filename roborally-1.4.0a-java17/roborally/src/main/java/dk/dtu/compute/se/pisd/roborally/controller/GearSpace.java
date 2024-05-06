b6a937e7e29daf319121d4c1fc39f04d3f671b21
JalokinBo <144294697+JalokinBo@users.noreply.github.com>
2024-05-06 20:39:34 +0200

    tags 5 og oprydning

--- roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/GearSpace.java
+++ roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/GearSpace.java
@@ -3,25 +3,33 @@ package dk.dtu.compute.se.pisd.roborally.controller;
 import dk.dtu.compute.se.pisd.roborally.model.Heading;
 import dk.dtu.compute.se.pisd.roborally.model.Space;
 
+/**
+ * @author Nikolaj
+ */
 public class GearSpace extends FieldAction{
-    private Heading heading;
     private String gearType;
 
-    public Heading getHeading(){
-        return heading;
-    }
-    
-    public void setHeading(Heading heading){
-        this.heading = heading;
-    }
-
+    /**
+     * @author Nikolaj 
+     * @return gearType
+     * returns the string gearType, which determines if the gear spins LEFT or RIGHT.
+     */
     public String getGearType(){
         return gearType;
     }
 
+    /**
+     * @author Nikolaj
+     * @param gearType
+     * Sets the String gearType. Only 'LEFT' and 'RIGHT' is allowed.
+     */
     public void setGearType(String gearType){
         this.gearType = gearType;
     }
+
+    /**
+     * currently not used. Will be used in future iterations.
+     */
     @Override
     public boolean doAction(GameController gameController, Space space) {
         // TODO Auto-generated method stub
