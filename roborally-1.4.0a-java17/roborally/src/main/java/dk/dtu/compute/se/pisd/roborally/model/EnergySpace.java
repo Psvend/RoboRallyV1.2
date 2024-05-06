fc67dc5bd7810b3848179120756c207ff528f461
Benjamin Philip Bakkelev <s235097@dtu.dk>
2024-05-06 11:50:45 +0200

    hmm

--- roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/model/EnergySpace.java
+++ roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/model/EnergySpace.java
@@ -1,57 +1,15 @@
 package dk.dtu.compute.se.pisd.roborally.model;
 
-
-import dk.dtu.compute.se.pisd.roborally.controller.FieldAction;
-import dk.dtu.compute.se.pisd.roborally.controller.GameController;
-import dk.dtu.compute.se.pisd.designpatterns.observer.Subject;
-import dk.dtu.compute.se.pisd.roborally.model.Heading;
-import dk.dtu.compute.se.pisd.roborally.model.Player;
-import dk.dtu.compute.se.pisd.roborally.model.Space;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
-import javafx.scene.layout.StackPane;
-
-
-
-/*TROR DER ER NOGET GALT. MANGLER EN CONNECTION TIL NÅR RESERVEN PRINTES (L. 108 PLAYERVIEW), OPDATERER IKKE
-*NÅR EN SPILLER LANDER PÅ ET ENERGYSPACE....
-*/
-
-
-
-/**
- * @author Petrine
- * @param EnergySpace
- * The design of the energy space. Is connected to a player, by checking if the player is on its field.
- * If the player is on the field, and the space has a cube, the player will get one added to its energyReserve
- */
-
-
-public class EnergySpace extends Space {
-    
+public class EnergySpace extends Space{
     public boolean hasEnergyCube;
-
     public EnergySpace(Board board, int x, int y) {
-        super(board, x, y);  //space on the board
-        this.hasEnergyCube = true;  //initialiserer space med cube
-        
-    }
-
-}
-
-
-    //kan fylde den op med en cube igen hvis ønsket
-    public void resetEnergyCube() {
+        super(board, x, y);
         this.hasEnergyCube = true;
     }
-
-
-    //kan tjekke om den er fuld el. tom
     public boolean hasEnergyCube() {
-        return this.hasEnergyCube;
+        return hasEnergyCube;
     }
-
-
-
+    public boolean setHasEnergyCube(boolean hasEnergyCube) {
+        return hasEnergyCube = hasEnergyCube;
+    }
 }
-*/
