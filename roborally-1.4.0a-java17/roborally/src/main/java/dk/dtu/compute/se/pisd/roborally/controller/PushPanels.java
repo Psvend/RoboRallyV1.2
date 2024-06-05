6ceba1eac219c8912899e1cb2b430560775b3462
JalokinBo <144294697+JalokinBo@users.noreply.github.com>
2024-06-06 01:42:10 +0200

    Attribute class (done)

--- roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/PushPanels.java
+++ roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/PushPanels.java
@@ -6,6 +6,22 @@ public class PushPanels extends FieldAction{
     private Heading heading;
     private int pushRegisters[];
 
+    public void setHeading(Heading heading) {
+        this.heading = heading;
+    }
+
+    public Heading getHeading(){
+        return heading;
+    }
+
+    public void setRegisters(int pushRegisters[]) {
+        this.pushRegisters = pushRegisters;
+    }
+
+    public int[] getRegisters() {
+        return pushRegisters;
+    }
+    
     @Override
     public boolean doAction(GameController gameController, Space space) {
         // TODO Auto-generated method stub
