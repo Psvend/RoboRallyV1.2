d334063b153e90dbb20bc66f4c9be7e74e6575e4
JalokinBo <144294697+JalokinBo@users.noreply.github.com>
2024-06-05 20:39:11 +0200

    Attribute class for PushPanels

---
+++ roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/controller/PushPanels.java
@@ -0,0 +1,15 @@
+package dk.dtu.compute.se.pisd.roborally.controller;
+import dk.dtu.compute.se.pisd.roborally.model.Heading;
+import dk.dtu.compute.se.pisd.roborally.model.Space;
+
+public class PushPanels extends FieldAction{
+    private Heading heading;
+    private int pushRegisters[];
+
+    @Override
+    public boolean doAction(GameController gameController, Space space) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'doAction'");
+    }
+
+}
