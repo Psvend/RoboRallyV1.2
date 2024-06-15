20083d7ebc287e9d6592f5472fb16b65ddde8800
JalokinBo <144294697+JalokinBo@users.noreply.github.com>
2024-06-15 04:23:54 +0200

    sortTokens

--- roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/model/Player.java
+++ roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/model/Player.java
@@ -176,8 +176,8 @@ public class Player extends Subject {
         }
 
         List<Integer> tempTokens = new ArrayList<>();
-        for(int k = 0; k<= tokens.size(); k++){
-            for(int j = 0; j <= tokens.size(); j++){
+        for(int k = 0; k<= tokens.size()-1; k++){
+            for(int j = 0; j <= tokens.size()-1; j++){
                 if(tokens.get(j) == (compare.get(k))) {
                     tempTokens.add(tokens.get(j));
                 } else {}
