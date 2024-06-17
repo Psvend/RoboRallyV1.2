4ecf5496cfbb66aaade1802617cad00aa19538e2
Benjamin Philip Bakkelev <s235097@dtu.dk>
2024-06-17 09:46:46 +0200

    yes

--- roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/network/ServiceClient.java
+++ roborally-1.4.0a-java17/roborally/src/main/java/dk/dtu/compute/se/pisd/roborally/network/ServiceClient.java
@@ -1,11 +1,15 @@
 package dk.dtu.compute.se.pisd.roborally.network;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dk.dtu.compute.se.pisd.roborally.model.Player;
+
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.TimeUnit;
 
 public class ServiceClient implements HttpRequestService{
     private HttpRequestService httpRequestService;
@@ -15,16 +19,27 @@ private static final HttpClient httpClient = HttpClient.newBuilder()
         .version(HttpClient.Version.HTTP_2)
         .build();
 
-public static String getProducts() throws Exception {
+public static String getPlayer() throws Exception {
     HttpRequest request = HttpRequest.newBuilder()
             .GET()
-            .uri(URI.create("http://localhost:8080/products"))
-            .setHeader("User-Agent", "Product Client")
-            .header("Content-Type", "application/json")
+            .uri(URI.create("http://localhost:3306/api/players"))
             .build();
     CompletableFuture<HttpResponse<String>> response =
             httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
-    String result = response.thenApply((r)->r.body()).get(5, TimeUnit.SECONDS);
+    String result = response.thenApply((r)-> {
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            List<String > players = objectMapper.readValue(r.body(), List.class);
+            for (String player : players) {
+                Player player1 = objectMapper.readValue(player, Player.class);
+                return player1.toString();
+
+            }
+            return r.body();
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }).join();
     return result;
 }
     @Override
