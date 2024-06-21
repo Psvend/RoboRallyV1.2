2cdd548ed9709ba96abb8896f032b2759d66535b
JalokinBo <Nikolajbo@gmail.com>
2024-06-21 13:45:45 +0200

    New Java files created for each type of object we test.

---
+++ roborally-1.4.0a-java17/roborally/src/test/java/dk/dtu/compute/se/pisd/roborally/controller/BoardElementsTest.java
@@ -0,0 +1,32 @@
+package dk.dtu.compute.se.pisd.roborally.controller;
+
+import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit5.ApplicationTest;
+
+
+public class BoardElementsTest extends ApplicationTest {
+    @Test
+    void Wall_Test(){
+
+    }
+
+    @Test
+    void Pitfall_Test(){
+
+    }
+
+    @Test
+    void ConveyorBelt_Test(){
+
+    }
+
+    @Test
+    void GearSpace_Test(){
+
+    }
+
+    @Test
+    void EnergySpace_Test(){
+
+    }   
+}
---
+++ roborally-1.4.0a-java17/roborally/src/test/java/dk/dtu/compute/se/pisd/roborally/controller/PlayerMovementTest.java
@@ -0,0 +1,36 @@
+package dk.dtu.compute.se.pisd.roborally.controller;
+
+import org.junit.jupiter.api.Test;
+import org.testfx.framework.junit5.ApplicationTest;
+
+public class PlayerMovementTest extends ApplicationTest{
+    @Test
+    void OutsideOfBoard_Test(){
+
+    }
+
+    @Test
+    void PushPlayerForward_Test(){
+
+    }
+
+    @Test
+    void PushPlayerBackwards_Test(){
+
+    }
+
+    @Test
+    void RespawnPush_Test(){
+
+    }
+
+    @Test
+    void CheckPoint_Test(){
+
+    }
+
+    @Test
+    void Winner_Test(){
+
+    }
+}
